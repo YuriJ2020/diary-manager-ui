29dbdaf5f6827f51575ede325211c52a1ea28b57
YuriJ <yurika.t.ji@gmail.com>
2021-07-12 20:03:16 +1000

    - Hora yappari;

--- src/settings/backend.js
+++
@@ -1,9 +0,0 @@
-const backend = {
-  endpoints: {
-    signup: "http://localhost:9999/api/signup",
-    login: "http://localhost:9999/api/login",
-    diary: "http://localhost:9999/api/diary",
-  },
-};
-
-export default backend;
--- src/settings/frontend.js
+++
@@ -1,7 +0,0 @@
-const frontend = {
-  moment: {
-    format: 'YYYY-MM-DD hh:mm:ssa Z',
-  },
-};
-
-export default frontend;
